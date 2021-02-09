0555a74fe409fa84cbf08f69fd1bfe426c757e60
vegebond <allenmontgomery44@gmail.com>
2021-02-09 14:00:31 -0600

    Update labelz.py

--- labelz.py
+++ labelz.py
@@ -2,7 +2,7 @@
 
 #MIT License
 #
-#Copyright (c) 2021 vegebond
+#Copyright (c) 2021 Allen D. Montgomery
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
